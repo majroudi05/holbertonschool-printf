9ffe60fa8bd944259bf6a203c315e91c9131d3b3
majroudi05 <majroudi05@gmail.com>
2023-07-31 04:56:44 -0700

    fixed file description

--- 2-specifier.c
+++ 2-specifier.c
@@ -1,16 +1,16 @@
 #include"main.h"
 /**
  *get_specifier - find the format of specifier
- *@s: the format of specifier
+ *@c: the format of specifier
  *Return: the number of bytes printed
  **/
 int (*get_specifier(const char *c))(va_list ap)
 {
-  int i;
-  specifier_t arry[] = {
-			{"c", print_char};
-			{"s", print_string};
-			{"%", print_precent};
-			{NULL, NULL}
-  };
+int i;
+specifier_t arry[] = {
+{"c", print_char};
+{"s", print_string};
+{"%", print_precent};
+{NULL, NULL}
+};
 }
--- 3-print_char.c
+++ 3-print_char.c
@@ -1,8 +1,12 @@
-#include "mainh"
-
+#include "main.h"
+/**
+ *print_char - print charachters
+ *@ap: input value
+ *Return: length of characters printed
+ **/
 int print_char(va_list ap)
 {
-	char c = va_arg(ap, int);
-	_putchar(c);
-	return(1);
+char c = va_arg(ap, int);
+_putchar(c);
+return(1);
 }
