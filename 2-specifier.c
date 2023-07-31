4c39490d283079451c53aea1049046723e3828c8
majroudi05 <majroudi05@gmail.com>
2023-07-31 03:57:34 -0700

    printf

---
+++ 2-specifier.c
@@ -0,0 +1,7 @@
+#include"main.h"
+/**
+ *get_specifier - find the format of specifier
+ *@s: the format of specifier
+ *Return: the number of bytes printed
+ **/
+int (*get_specifier(char *s))(va_list ap,
